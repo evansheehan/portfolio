f67c2095ff37cd1cbcfa869bb17b183d96b78cca
evan-sheehan <67272025+evan-sheehan@users.noreply.github.com>
2022-05-06 23:21:32 -0400

    Initial

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="relative bg-slate-800 p-8 rounded-md text-slate-300">
+	<slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-lg font-medium">Hello!</h1>
+<div class="before:bg-slate-600">
+	<p>My name is Evan Sheehan</p>
+	<p>Hopefull something cool will be here in the future!</p>
+</div>
