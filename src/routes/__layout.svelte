20ddfebfe22ce2d13dbc147a8d3d18fdd393c04c
evan-sheehan <67272025+evan-sheehan@users.noreply.github.com>
2022-05-07 00:26:15 -0400

    Add two main states to the page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,36 @@
-<script>
-	import '../app.css';
+<script context="module" lang="ts">
+	enum State {
+		Start,
+		Expanded
+	}
+	export const state = writable(State.Start);
 </script>
 
-<main class="relative bg-primary-mid p-8 rounded-md text-slate-300">
+<script lang="ts">
+	import { writable } from 'svelte/store';
+	import '../app.css';
+
+	let mainEl: HTMLElement;
+
+	$: expandState($state);
+	const expandState = (newState: State) => {
+		console.log('Init expand state');
+		if (newState !== State.Expanded) return;
+	};
+</script>
+
+<main
+	bind:this={mainEl}
+	on:click={() => {
+		const mainElBoundsBefore = mainEl.getBoundingClientRect();
+		state.set(State.Expanded);
+		const mainElBoundsAfter = mainEl.getBoundingClientRect();
+	}}
+	class="relative bg-primary-mid transition  select-none  p-8 rounded-md text-slate-300 {$state ===
+	State.Expanded
+		? 'h-full w-full'
+		: 'hover:scale-105 hover:ring-2 ring-0 ring-accent-deep cursor-pointer'}"
+>
 	<slot />
 </main>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { state } from './__layout.svelte';
+</script>
+
 <h1 class="text-3xl font-bold text-accent-deep">Hello!</h1>
 <p class="text-accent-mid">My name is Evan Sheehan</p>
 <p class="text-accent-mid">Hopefully something cool will be here in the future!</p>
