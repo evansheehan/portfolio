695123fe63de2e683cac7dd7c2e6ff4adb3012f9
Evan Sheehan <awubis@gmail.com>
2022-05-22 12:53:12 -0400

    Add a testing  page

---
+++ src/routes/test.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+	export async function load() {
+		const isDevEnv = import.meta.env.DEV;
+		if (isDevEnv) {
+			return {
+				status: 200
+			};
+		} else {
+			return {
+				status: 401,
+				message: 'Uh oh!'
+			};
+		}
+	}
+</script>
+
+<script>
+	import Nav from '$lib/nav.svelte';
+</script>
+
+<Nav />
