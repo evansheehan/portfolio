46894df6a1a8d66727418f3fff23870b8d51af94
Evan Sheehan <awubis@gmail.com>
2022-07-02 14:40:27 -0400

    Add canvas repeated background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
 	import '../app.css';
+
+	let canvasEl: HTMLCanvasElement;
+
+	const populateCanvas = (
+		canvas: HTMLCanvasElement,
+		{ mouseX, mouseY }: { mouseX: number; mouseY: number }
+	) => {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		const ctx = canvas.getContext('2d');
+		if (!ctx) return;
+		ctx.fillStyle = 'rgba(256, 256, 256, 0.05)';
+
+		for (let i = 0; i < canvas.width + 10; i = i + 10) {
+			for (let j = 0; j < canvas.height + 10; j = j + 10) {
+				// if (i - mouseX < 20) ctx.fillRect(i, j, 10, 10);
+				// else if (j - mouseY < 20) ctx.fillRect(i, j, 10, 10);
+				ctx.fillRect(i, j, 5, 5);
+			}
+		}
+		// ctx.fillText(canvas.getBoundingClientRect().width.toString(), 50, 50, 500);
+	};
 </script>
 
+<svelte:window on:resize={() => populateCanvas(canvasEl, { mouseX: 0, mouseY: 0 })} />
+
 <main
-	class="flex items-center justify-center bg-gradient-to-br from-slate-800 to-black w-full overflow-y-auto h-full p-4"
+	class="flex items-center overflow-hidden justify-center bg-gradient-to-br from-slate-800 to-black w-full h-full p-4"
 >
+	<canvas use:populateCanvas={{ mouseX: 0, mouseY: 0 }} bind:this={canvasEl} />
 	<slot />
 </main>
 
