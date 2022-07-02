94fdd2945a899c4e491da9db76c993cf860a943f
Evan Sheehan <awubis@gmail.com>
2022-07-01 23:53:23 -0400

    Move expandable components out of utils folder

---
+++ src/lib/Expandable.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let backgroundColorClass: string;
+	export let title: string;
+</script>
+
+<button
+	on:click
+	class="text-white shadow-sm hover:shadow-lg active:shadow-inner active:scale-90 ease-in-out hover:scale-150 transition w-full hover:z-10 h-full font-semibold flex items-center justify-center p-8 rounded-lg {backgroundColorClass}"
+>
+	<span>
+		{title}
+	</span>
+</button>
