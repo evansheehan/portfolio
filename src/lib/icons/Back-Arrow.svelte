c23ce2d85b682103dc2141b3e68a5da7b2cb4bbc
Evan Sheehan <awubis@gmail.com>
2022-07-01 23:53:30 -0400

    Add back arrow svg

---
+++ src/lib/icons/Back-Arrow.svelte
@@ -0,0 +1,16 @@
+<svg
+	on:click
+	xmlns="http://www.w3.org/2000/svg"
+	aria-hidden="true"
+	role="img"
+	class="w-6 h-6 cursor-pointer"
+	preserveAspectRatio="xMidYMid meet"
+	viewBox="0 0 24 24"
+	><g
+		fill="none"
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"><path d="m8 5l-5 5l5 5" /><path d="M3 10h8c5.523 0 10 4.477 10 10v1" /></g
+	></svg
+>
